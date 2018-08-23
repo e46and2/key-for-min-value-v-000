1277832a04f4fc68ef03bb9f52238d9910251e03
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-08-23 18:01:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
-end
+  name_a = name_hash.to_a
+
+  d_value= 10000
+  d_key= 0
+
+  name_a.each do |i|
+
+      if i[1] < d_value
+        d_value = i[1]
+        d_key = i[0]
+      end
+
+   end
+
+  return d_key
+
+end  
